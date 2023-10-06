aba8cec3611b9294ba04ea101dd390e820ac0048
Richkum <kumatum2@gmail.com>
2023-10-06 13:49:05 +0100

    starting my js

---
+++ asset/script/script.js
@@ -0,0 +1,6 @@
+const firstString = document.querySelector('#first');
+const secondString = document.querySelector('#second');
+const message = document.querySelector('#message');
+const submit = document.querySelector('#submit');
+
+
